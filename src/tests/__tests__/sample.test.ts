3bc80867732803e1d8124cef823877ac70e14ece
padi-dev-linhnd <linhnd@paditech.com>
2023-02-01 15:45:20 +0700

    test_jest

--- src/tests/__tests__/sample.test.ts
+++ src/tests/__tests__/sample.test.ts
@@ -5,3 +5,15 @@ describe('sample', () => {
     expect(1 + 1).toEqual(2)
   })
 })
+
+import { AssessmentController } from '../../app/controllers/assessment.controller'
+import AssessmentRepository from '../../app/repositories/assessment.repository'
+import Assessment from '../../models/entities/assessment.entity'
+
+it('list_assessment[0].id = 1', async () => {
+  const assessmentController = new AssessmentController(new AssessmentRepository(Assessment))
+
+  const list_assessment = await assessmentController.getAllWhere({})
+
+  expect(list_assessment[0].id).toEqual(1)
+})
